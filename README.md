9312a324cecb30930f811de6fc547e803c9c0291
Wissem Sahli <w.g.s.o.m.2003@gmail.com>
2026-01-27 22:31:59 +0100

    getting_oaca_invoices

--- README.md
+++ README.md
@@ -6,8 +6,33 @@ nb : raw_data : read-only
 Normalize Image Format : Convert ALL .tif → .png 
  using python (Pillow) 
  ( creation of a function :
- convertor( inputfolderpath , outputfolderpath, desired name for the file-"file" is default)
+ convertor_tif_png( inputfolderpath , outputfolderpath, desired name for the file-"file" is default)
  )
 Split Documents by ROLE
 
 split the all folder to test-val-train using python ( splitfolder) ratio 0.7 - 0.15 -0.15
+
+annotation : 125 invoice , Required fields.txt
+( Invoice number
+Date
+Total amount
+Currency
+Supplier name (if visible) ) 
+
+each invoice gets a JSON exemple : 
+{
+  "invoice_number": "INV-2024-019",
+  "invoice_date": "2024-12-10",
+  "total_amount": "1240.50",
+  "currency": "TND",
+}
+store in data/annotations/ner/
+nb : Filename must match image name.
+
+Got 150 invoices from OACA they all clean adn structuted i ll start using them 
+
+Problem 150 in 1 pdf : each page is an invoice ! 
+
+So pdf => .png files 
+
+using python (pdf2image ) agian create a fuction named convertor_pdf_png, downloading poppler (So python could count the pages) and copy the path of its bin ( insdie the liraby folder ) after unexrating from .zip ( downlink : https://github.com/oschwartz10612/poppler-windows/releases/ ) 
--- python/convert_img.py
+++ python/convert_img.py
@@ -1,12 +1,12 @@
 import os 
 from PIL import Image
-
+from pdf2image import convert_from_path, pdfinfo_from_path
 
 # input => the folder where .tif files are
 # ouput => the folder where .png will be 
 # name => new file's name, "file" is set by default ! 
 
-def convertor(input,output,name = "file"):
+def convertor_tif_png(input,output,name = "file"):
 
     input = input.replace("\\", "/")
     output = output.replace("\\", "/")
@@ -39,4 +39,50 @@ def convertor(input,output,name = "file"):
 path_in = r"C:\Users\wgsom\Desktop\AI_Project\data\raw\public"
 path_out = r"C:\Users\wgsom\Desktop\AI_Project\data\images\non_invoices\all"
 
-convertor(path_in,path_out,"non_invoices")
+
+def convertor_pdf_png(input,output,name = "file"):
+
+    input = input.replace("\\", "/")
+    output = output.replace("\\", "/")
+    
+    poppler = r"C:\Program Files\poppler-25.12.0\Library\bin"
+
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    if not os.path.exists(input):
+        print(f"❌ ERROR: The PDF file was not found at that path!")
+        return
+    
+    try:
+        info = pdfinfo_from_path(input, poppler_path=poppler)
+        total_pages = info["Pages"]
+        print(f"--- Process Started ---")
+        print(f"Total pages to convert: {total_pages}")
+
+        # Convert pages one by one in a loop
+        for i in range(1, total_pages + 1):
+            page = convert_from_path(
+                input, 
+                dpi=300, 
+                first_page=i, 
+                last_page=i, 
+                poppler_path=poppler
+            )
+            
+            img_name = f"{name}_{i}.png"
+            save_path = os.path.join(output, img_name)
+            page[0].save(save_path, 'PNG')
+            
+            print(f"✅ [{i}/{total_pages}] Saved: {img_name}")
+
+        print(f"--- Finished! ---")
+
+    except Exception as e:
+        print(f"❌ ERROR: {str(e)}")
+    
+
+path_in =r"C:\Users\wgsom\Desktop\AI_Project\OACA.pdf"
+path_out = r"C:\Users\wgsom\Desktop\AI_Project\data_oaca\invoices"
+
+convertor_pdf_png(path_in,path_out,"invoice")
